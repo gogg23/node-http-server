58ed97dfbcd337037d8bd8f50059d088a4299fcc
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2025-05-15 12:03:33 +0300

    commit

---
+++ .history/content_20250515105645.js
@@ -0,0 +1,20 @@
+import guitars from './data.js';
+
+export const createList = (guitars) => `
+  <h2>My Guitars</h2>
+  <ul>
+    ${guitars.map(createListItem).join('\n')}
+  </ul>
+`;
+
+const createListItem = ({ id, make, model }) =>
+  `<li><a href="?id=${id}">${make} ${model}</a></li>`;
+
+export function getGuitarContent(guitar) {
+  if (guitar) {
+    return `<h2>${guitar.make} ${guitar.model}</h2>
+    <p><a href="/delete/${guitar.id}">Delete</a>`;
+  } else {
+    return '<p>Guitar does not exist</p>';
+  }
+}
