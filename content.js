ffa139d398d6fcdced997ea41eb974f8ec90f192
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2025-05-16 12:22:49 +0300

    commit

--- content.js
+++ content.js
@@ -8,6 +8,19 @@ export const createList = (guitars) => `
 const createListItem = ({ id, make, model }) =>
   `<li><a href="?id=${id}">${make} ${model}</a></li>`;
 
+export const getForm = () => `
+<form method="POST" action="/add">
+<div>
+     Make: <input type="text" name="guitar_make" />
+</div>
+<div>
+     Model: <input type="text" name="guitar_model" />
+</div>
+<div>
+     <button type="submit">Save</button>
+</div>
+</form>`;
+
 export function getGuitarContent(guitar) {
   if (guitar) {
     return `<h2>${guitar.make} ${guitar.model}</h2>
@@ -16,3 +29,16 @@ export function getGuitarContent(guitar) {
     return '<p>Guitar does not exist</p>';
   }
 }
+
+export const view = (content) => `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Guitars</title>
+</head>
+<body style="font-size:1.3rem">
+  ${content}
+</body>
+</html>`;
