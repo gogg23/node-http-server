70ba5c7279a673f1a9c86748f8b6940432fdd9e1
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2025-05-20 19:40:53 +0300

    commit

--- data.js
+++ data.js
@@ -1,5 +1,17 @@
-export default [
-  { id: 1, make: 'Fender', model: 'strat' },
-  { id: 2, make: 'PRS', model: 'Starla' },
-  { id: 3, make: 'Gibson', model: 'Les Paul' },
+let id = 1;
+
+function newId() {
+  return id++;
+}
+
+const guitars = [
+  { newId, make: 'Fender', model: 'strat' },
+  { newId, make: 'PRS', model: 'Starla' },
+  { newId, make: 'Gibson', model: 'Les Paul' },
 ];
+
+export const saveGuitar = () => guitars;
+export function saveGuitar(guitar) {
+  guitar.id = newid();
+  guitars.push(guitar);
+}
